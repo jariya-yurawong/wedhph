31757dd9bf0b47cbf25b8050aed13522f0e6bd31
jariya-yurawong <1631051541316@rmutr.ac.th>
2021-05-31 13:06:38 +0700

    set1
    set1-4

---
+++ aa1.php
@@ -0,0 +1,30 @@
+
+       <!DOCTYPE html>
+       <html>
+           <head>
+                  
+                   <title>format</title>
+                   <meta charset="UTF-8">
+                   <meta name ="viewport"content="width=device-width,initial-scale=1.0">
+                   <link rel ="stylesheet" type="text/css" href="mystyle.css">
+            </head>
+            </head>
+                <body>
+                <form action="final2.1.php" method="post">
+       
+             num1 : <input type="number" name="num1"  required >
+             <br/><br/>
+             num2 : <input type="number" name="num2"  required>
+             <br/><br/>
+             num3 : <input type="number" name="num3" required>
+             <br/><br/>
+             num4 : <input type="number" name="num4" required>
+             <br/><br/>
+             num5 : <input type="number" name="num5" required>
+             <br/><br/>
+             <input type="submit">
+       </form>
+                </body>
+              
+       </html>
+        
